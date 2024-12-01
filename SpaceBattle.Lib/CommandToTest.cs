76df19d9f679a8134937b780b60faa0d624820c2
Mawak0 <a32221580@gmail.com>
2024-12-01 12:02:27 +0600

    test: CommandToTest added

---
+++ SpaceBattle.Lib/CommandToTest.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceBattle.Lib;
+
+public class CommandToTest : ICommand
+{
+    private int cnt;
+
+    public void Execute()
+    {
+        cnt++;
+    }
+
+    public int getCount()
+    {
+        return cnt;
+    }
+}
