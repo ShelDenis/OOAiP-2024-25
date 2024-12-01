965f7cbf158d7fdb83f4b882d22c57f7e861cf56
Mawak0 <a32221580@gmail.com>
2024-12-01 14:20:21 +0600

    test: Rotate tests added

---
+++ SpaceBattle.Lib.Tests/RotateTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class RotateCommandTests
+{
+
+    [Fact]
+    public void RotateCommandPositive()
+    {
+        var angle1 = new Angle(30);
+        var angle2 = new Angle(60);
+
+        var mockRotating = new Mock<IRotate>();
+        mockRotating.SetupGet(v => v.PositionAngle).Returns(angle1).Verifiable();
+        mockRotating.SetupGet(v => v.VelocityAngle).Returns(angle2).Verifiable();
+
+        ICommand command = new RotateCommand(mockRotating.Object);
+        command.Execute();
+
+        mockRotating.VerifySet(v => v.PositionAngle = angle1 + angle2, Times.Once);
+    }
+}
