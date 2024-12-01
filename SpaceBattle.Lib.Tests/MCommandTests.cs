4b7aaed4f30f535d06f3c232bbfe969d29437eb7
Mawak0 <a32221580@gmail.com>
2024-12-01 12:02:54 +0600

    test: MCommand tests added

---
+++ SpaceBattle.Lib.Tests/MCommandTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+namespace SpaceBattle.Lib.Tests;
+
+public class MCommandTests
+{
+    [Fact]
+    public void MCommandTest()
+    {
+        var ctt1 = new CommandToTest();
+        var ctt2 = new CommandToTest();
+        List<ICommand> lst = [ctt1, ctt2];
+        var mc = new MCommand(lst);
+        mc.Execute();
+
+        Assert.Equal(2, ctt1.getCount() + ctt2.getCount());
+    }
+}
