a9263d945089e8e0241d597400229beef53733c8
Mawak0 <a32221580@gmail.com>
2024-12-01 16:07:53 +0600

    test: StopRotateCommand tests added

---
+++ SpaceBattle.Lib.Tests/StopRotateCommandTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class StopRotateCommandTests
+{
+    [Fact]
+    public void StopRotateCommandTest()
+    {
+        var queue = new Queue<ICommand>();
+
+        var velocityAngle = new Angle(45);
+        var positionAngle = new Angle(0);
+
+        var gameObject = new Dictionary<string, object>();
+        gameObject["PositionAngle"] = positionAngle;
+
+        var startRotateOrder = new Mock<StartRotateOrder>();
+        startRotateOrder.SetupGet(m => m.VelocityAngle).Returns(velocityAngle);
+        startRotateOrder.SetupGet(m => m.GameObject).Returns(gameObject);
+
+        var startRotateCmd = new StartRotateCommand(startRotateOrder.Object, queue);
+        var stopRotateCmd = new StopRotateCommand(gameObject);
+
+        startRotateCmd.Execute();
+
+        queue.Dequeue().Execute();
+
+        stopRotateCmd.Execute();
+        queue.Dequeue().Execute();
+
+        Assert.Throws<InvalidOperationException>(() => queue.Dequeue().Execute());
+    }
+}
