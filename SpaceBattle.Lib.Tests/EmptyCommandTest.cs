6ac825eac7ae9a8eae6f97699542ecc639fe5cbf
Mawak0 <a32221580@gmail.com>
2024-12-01 11:28:23 +0600

    test: EmptyCommand tests added

---
+++ SpaceBattle.Lib.Tests/EmptyCommandTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+namespace SpaceBattle.Lib.Tests;
+
+public class EmptyCommandTest
+{
+    [Fact]
+    public void Execute_EmptyCommand()
+    {
+        var empty_cmd = new EmptyCommand();
+        empty_cmd.Execute();
+    }
+}
+
