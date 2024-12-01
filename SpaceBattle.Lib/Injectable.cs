42af7c2f7104ad452ef6f1d9af013385edac4a14
Mawak0 <a32221580@gmail.com>
2024-12-01 14:02:50 +0600

    feat: InjectableCommand added

--- SpaceBattle.Lib/Injectable.cs
+++ SpaceBattle.Lib/Injectable.cs
@@ -4,3 +4,16 @@ public interface Injectable
 {
     void Inject(ICommand cmd);
 }
+
+public class InjectableCommand : ICommand, Injectable
+{
+    private ICommand _cmd = new EmptyCommand();
+    public void Execute()
+    {
+        _cmd.Execute();
+    }
+    public void Inject(ICommand cmd)
+    {
+        _cmd = cmd;
+    }
+}
