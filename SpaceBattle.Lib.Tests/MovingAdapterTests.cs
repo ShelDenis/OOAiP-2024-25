339048b18e0533f614cf5a96cb1ecca2d2e6b13b
Mawak0 <a32221580@gmail.com>
2024-12-01 15:32:44 +0600

    test: MovingAdapter tests added

---
+++ SpaceBattle.Lib.Tests/MovingAdapterTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+namespace SpaceBattle.Lib.Tests;
+
+public class MovingAdapterTests
+{
+    [Fact]
+    public void AdapterDefaultValuesTest()
+    {
+        var dictionary = new Dictionary<string, object>();
+        var adapter = new MovingAdapter(dictionary);
+
+        Assert.Equal(default(Vector), adapter.Position);
+        Assert.Equal(default(Vector), adapter.Velocity);
+    }
+}
