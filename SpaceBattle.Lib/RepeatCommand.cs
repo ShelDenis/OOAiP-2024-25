4ff674c2263a1aa6d264e63ccc21055a0965705c
Mawak0 <a32221580@gmail.com>
2024-12-01 13:58:22 +0600

    feat: RepeatCommand added

---
+++ SpaceBattle.Lib/RepeatCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib;
+
+public class RepeatCommand : ICommand
+{
+    private readonly Queue<ICommand> _q;
+    private readonly ICommand _toRepeat;
+    public RepeatCommand(Queue<ICommand> q, ICommand toRepeat)
+    {
+        _q = q;
+        _toRepeat = toRepeat;
+    }
+
+    public void Execute()
+    {
+        _q.Enqueue(_toRepeat);
+    }
+}
