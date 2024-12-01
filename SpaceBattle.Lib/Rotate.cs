a00e4653b1d8e71524cd894f1d9b314b73513d17
Mawak0 <a32221580@gmail.com>
2024-12-01 14:18:32 +0600

    feat: RotateCommand added

--- SpaceBattle.Lib/Rotate.cs
+++ SpaceBattle.Lib/Rotate.cs
@@ -4,3 +4,16 @@ public interface IRotate
     Angle PositionAngle { get; set; }
     Angle VelocityAngle { get; }
 }
+
+public class RotateCommand : ICommand
+{
+    private readonly IRotate obj;
+    public RotateCommand(IRotate obj)
+    {
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        obj.PositionAngle += obj.VelocityAngle;
+    }
+}
