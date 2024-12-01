16934f0bda39fc6f0421a637f68ee9d1f10195e3
Mawak0 <a32221580@gmail.com>
2024-12-01 14:38:27 +0600

    feat: RotateAdapter added

---
+++ SpaceBattle.Lib/RotateAdapter.cs
@@ -0,0 +1,38 @@
+﻿namespace SpaceBattle.Lib;
+
+public class RotateAdapter : IRotate
+{
+    private readonly IDictionary<string, object> _dictionary;
+
+    public RotateAdapter(IDictionary<string, object> dictionary)
+    {
+        _dictionary = dictionary;
+    }
+
+    public Angle PositionAngle
+    {
+        get
+        {
+            if (_dictionary.TryGetValue(nameof(PositionAngle), out var value))
+            {
+                return (Angle)value;
+            }
+
+            return default;
+        }
+        set => _dictionary[nameof(PositionAngle)] = value;
+    }
+
+    public Angle VelocityAngle
+    {
+        get
+        {
+            if (_dictionary.TryGetValue(nameof(VelocityAngle), out var value))
+            {
+                return (Angle)value;
+            }
+
+            return default;
+        }
+    }
+}
