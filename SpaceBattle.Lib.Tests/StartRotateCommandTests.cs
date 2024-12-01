bc438f9d5b2841a3a2650b695fe5e4c4da394052
Mawak0 <a32221580@gmail.com>
2024-12-01 15:13:24 +0600

    test: StartRotateCommand tests added

---
+++ SpaceBattle.Lib.Tests/StartRotateCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using Xunit;
+namespace SpaceBattle.Lib.Tests;
+
+public class StartRotateCommandTests
+{
+    [Fact]
+    public void StartRotateCommandTest()
+    {
+        var queue = new Queue<ICommand>();
+
+        var velocityAngle = new Angle(45);
+        var positionAngle = new Angle(0);
+        var targetPositionAngle = new Angle(90);
+
+        var gameObject = new Dictionary<string, object>();
+        gameObject["PositionAngle"] = positionAngle;
+
+        var startRotateOrder = new Mock<StartRotateOrder>();
+        startRotateOrder.SetupGet(m => m.VelocityAngle).Returns(velocityAngle);
+        startRotateOrder.SetupGet(m => m.GameObject).Returns(gameObject);
+
+        var startRotateCmd = new StartRotateCommand(startRotateOrder.Object, queue);
+
+        startRotateCmd.Execute();
+
+        queue.Dequeue().Execute();
+        queue.Dequeue().Execute();
+
+        Assert.Equal(targetPositionAngle, gameObject["PositionAngle"]);
+    }
+}
