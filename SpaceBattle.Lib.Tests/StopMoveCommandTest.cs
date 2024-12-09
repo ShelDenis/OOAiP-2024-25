43d99a1ebcf1ef8603709e2f1b2051ffcd846304
Anastasia Essina <aaessina@yandex.ru>
2024-12-09 19:26:43 +0600

    test: StopMoveCommandTest added

---
+++ SpaceBattle.Lib.Tests/StopMoveCommandTest.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class StopMoveCommandTests
+    {
+        [Fact]
+        public void StopMoveCommandTest()
+        {
+            var injectableMock = new Mock<Injectable>();
+
+            var gameObject = new Mock<IDictionary<string, object>>();
+
+            gameObject.Setup(d => d["repeatableMove"]).Returns(injectableMock.Object);
+
+            var command = new StopMoveCommand(gameObject.Object);
+
+            command.Execute();
+
+            injectableMock.Verify(i => i.Inject(It.IsAny<EmptyCommand>()), Times.Once);
+        }
+    }
+}
